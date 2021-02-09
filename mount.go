eee3c2c2da82adb1f9c387d7b72f6a965c473bb1
Thomas Mitchell <klaue@live.com>
2021-02-08 19:09:58 -0500

    add TuneSecretsMount function to edit mounts

--- mount.go
+++ mount.go
@@ -197,3 +197,33 @@ func (c *Client) EnableSecretsMount(path string, config Mount) error {
 func (c *Client) DisableSecretsMount(path string) error {
 	return c.doRequest("DELETE", fmt.Sprintf("/sys/mounts/%s", path), nil, nil)
 }
+
+//TuneMountOptions are parameters to be sent to the Vault when editing the
+// configuration of a mount. Only non-empty values will be sent.
+type TuneMountOptions struct {
+	Description     string
+	DefaultLeaseTTL time.Duration
+	MaxLeaseTTL     time.Duration
+	Options         map[string]interface{}
+}
+
+//TuneSecretsMount updates the configuration of the mount at the given path.
+func (c *Client) TuneSecretsMount(path string, opts TuneMountOptions) error {
+	rawTuneMountOptions := struct {
+		Description     string                 `json:"description,omitempty"`
+		DefaultLeaseTTL int                    `json:"default_lease_ttl,omitempty"`
+		MaxLeaseTTL     int                    `json:"max_lease_ttl,omitempty"`
+		Options         map[string]interface{} `json:"options,omitempty"`
+	}{
+		Description:     opts.Description,
+		DefaultLeaseTTL: int(opts.DefaultLeaseTTL.Seconds()),
+		MaxLeaseTTL:     int(opts.MaxLeaseTTL.Seconds()),
+		Options:         opts.Options,
+	}
+
+	return c.doRequest("POST",
+		fmt.Sprintf("/sys/mounts/%s/tune", path),
+		rawTuneMountOptions,
+		nil,
+	)
+}
